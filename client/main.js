5b520361fa719c8c1540ea19daee6c746ced29e2
Blake Netzeband <blake.netzeband@gmail.com>
2017-08-30 20:57:49 -0600

    added basic view. updated font. started email signup pathway

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
-// Client entry point, imports all client code
 import '/imports/startup/client';
 import '/imports/startup/both';
+
+// temporary until https://github.com/meteor/meteor/issues/6846 is resolved
+import '@outboxcraft/beauter/beauter.min.css'
--- imports/ui/components/landingpage.jsx
+++ imports/ui/components/landingpage.jsx
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 
-const LandingPage = () => {
-  return (
-    <main className="lp">
-      <h1>In:Concert</h1>
-    </main>
-  )
-}
+export default class LandingPage extends Component {
+  constructor(){
+    super()
+    state = {
+      emailModalVis: false,
+    }
+  }
 
-export default LandingPage;
+  handleEmailClick () {
+    this.setState((prevState) => {
+      return {emailModalVis: !prevState}
+    })
+  }
+
+  handleFacebookClick () {
+    console.log('click')
+  }
+
+  handleTwitterClick () {
+    console.log('click')
+  }
+
+  render () {
+    return (
+      <main className="lp">
+        <div className="headerwrap">
+          <h1>In:Concert</h1>
+        </div>
+
+        <div>
+          <button onClick={this.handleEmailClick}><i className="fa fa-envelope-o" aria-hidden="true"></i>Signup with Email</button>
+          <button onClick={this.handleFacebookClick}><i className="fa fa-facebook-square" aria-hidden="true"></i>Signup with Facebook</button>
+          <button onClick={this.handleTwitterClick}><i className="fa fa-twitter-square" aria-hidden="true"></i>Signup with Twitter</button>
+        </div>
+
+        <div id="modal-email" className="modalbox-modal">
+          <div className="modalbox-modal-content">
+            <form>
+              <label for="username">Username</label>
+              <input type="text" id="username" name="username" />
+              <label for="email">Email</label>
+              <input type="text" id="email" name="email" />
+              <label for="password">Password</label>
+              <input type="text" id="password" name="password" />
+            </form>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
