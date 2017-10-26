738060852ab7b7e2d437a908e4544c0f64d1b773
netzebandito <blake.netzeband@gmail.com>
2017-10-25 18:52:15 -0600

    added tags to header. semantic changes. scrollmagic still busted :(

--- imports/ui/components/welcome.jsx
+++ imports/ui/components/welcome.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ScrollMagic from 'scrollmagic';
 import addIndicators from 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
-import { TweenMax } from "gsap";
+import { TweenMax } from 'gsap';
 
 export default class Welcome extends Component {
 	constructor(){
@@ -14,17 +14,20 @@ export default class Welcome extends Component {
 	}
 
 	drawCircles() {
-		const container = document.querySelector('.welcome');
-		const maxWidth = container.offsetHeight;
+		const container = document.querySelector('.scene');
+		const maxRadius = container.offsetHeight;
 		const svg = document.querySelector('svg');
 		const protoCircle = document.querySelector('circle');
+
 		// set svg attributes
-		svg.setAttribute('viewBox', `0 0 ${maxWidth} ${maxWidth}`);
-		svg.setAttribute('width', `${maxWidth}`);
-		svg.setAttribute('height', `${maxWidth}`);
+		svg.setAttribute('viewBox', `0 0 ${maxRadius} ${maxRadius}`);
+		svg.setAttribute('width', `${maxRadius}`);
+		svg.setAttribute('height', `${maxRadius}`);
+
 		// create circles and append to DOM
-		for (let clone, radius, i = parseInt(maxWidth/30); i >= 0; i--) {
-			radius = (maxWidth / 2) - (maxWidth / 30 * i);
+		for (let clone, radius, i = parseInt(maxRadius/30); i > 0; i--) {
+			radius = (maxRadius / 2) - (maxRadius / 30 * i);
+			console.log(radius)
 			if (radius < 0)
 				continue;
 			else {
@@ -42,16 +45,16 @@ export default class Welcome extends Component {
 		const scrollEndEle = document.querySelector('#scrollEnd');
 		const scrollEndRect = scrollEndEle.getBoundingClientRect();
 
-		for (var i = this.circles.length - 1; i >= 0; i--) {
+		for (var i = this.circles.length -1; i >= 0; i--) {
 			if (this.circles[i].id) {
-				const smScene = new ScrollMagic.Scene({
-					triggerElement: '#scrollStart',
-					duration: 500,
-					loglevel: 3,
-				})
-				.setPin(`#${this.circles[i].id}`)
-				.addIndicators()
-				.addTo(this.smController);
+				// const smScene = new ScrollMagic.Scene({
+				// 	triggerElement: '#scrollStart',
+				// 	duration: 500,
+				// 	loglevel: 3,
+				// })
+				// .setPin(`#${this.circles[i].id}`)
+				// .addIndicators()
+				// .addTo(this.smController);
 			}
 		}
 
@@ -66,18 +69,19 @@ export default class Welcome extends Component {
 	render () {
 		return (
 			<article id="sceneContainer">
-				<div className="scene welcome">
+				<div className="welcome">
 					<div className="headerwrap">
 						<h1>In:Concert</h1>
 					</div>
+				</div>
 
+				<div className="scene" id="scrollStart">
 					<svg viewBox="0 0 0 0" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg">
 						<circle cx="50%" cy="50%" r="0" stroke="#bbb" strokeWidth="3" fill="none"/>
 					</svg>
-
 					<i className="fa fa-long-arrow fa-3x fa-rotate-180" aria-hidden="true"></i>
 				</div>
-				<div className="scene" id="scrollStart"></div>
+
 				<div className="scene" id="scrollEnd"></div>
 			</article>
 		)
