a96a9a5550790f6862913a0af56d9a96b1c0855c
netzebandito <blake.netzeband@gmail.com>
2017-11-12 19:46:33 -0700

    draw circle in rendering stage

--- imports/ui/components/welcome.jsx
+++ imports/ui/components/welcome.jsx
@@ -5,65 +5,55 @@ import { TweenMax } from 'gsap';
 
 export default class Welcome extends Component {
 	constructor(){
-		super();
+		super();		
 		this.state = {
 			scrollMagicReady: false,
 		};
+
 		this.smController = new ScrollMagic.Controller();
+		this.maxRadius = window.innerHeight;
 		this.circles = [];
 	}
 
+	componentDidMount() {
+		if (this.state.scrollMagicReady == false) this.initScrollMagic();
+	}
+
+
 	drawCircles() {
-		const container = document.querySelector('.scene');
-		const maxRadius = container.offsetHeight;
-		const svg = document.querySelector('svg');
-		const protoCircle = document.querySelector('circle');
-
-		// set svg attributes
-		svg.setAttribute('viewBox', `0 0 ${maxRadius} ${maxRadius}`);
-		svg.setAttribute('width', `${maxRadius}`);
-		svg.setAttribute('height', `${maxRadius}`);
-
-		// create circles and append to DOM
-		for (let clone, radius, i = parseInt(maxRadius/30); i > 0; i--) {
-			radius = (maxRadius / 2) - (maxRadius / 30 * i);
-			console.log(radius)
-			if (radius < 0)
+		for (let radius, i = parseInt(this.maxRadius / 30); i > 0; i--) {
+			radius = parseInt((this.maxRadius / 2) - (this.maxRadius / 30 * i));
+			if (radius <= 0)
 				continue;
 			else {
-				clone = protoCircle.cloneNode(false);
-				clone.setAttribute('r', `${radius}`);
-				clone.setAttribute('id', `circle-${i}`);
-				svg.appendChild(clone);
+				this.circles[i] = React.createElement('circle', {
+					key:`circle-${i}`, id:`circle-${i}`, cx:'50%', cy:'50%', r:`${radius}`, stroke:'#bbb', strokeWidth:'3', fill:'none',
+				})
 			} 
 		}
 
-		this.circles = document.querySelectorAll('circle');
+		return this.circles;
 	}
 
 	initScrollMagic() {
-		const scrollEndEle = document.querySelector('#scrollEnd');
-		const scrollEndRect = scrollEndEle.getBoundingClientRect();
-
-		for (var i = this.circles.length -1; i >= 0; i--) {
-			if (this.circles[i].id) {
-				// const smScene = new ScrollMagic.Scene({
-				// 	triggerElement: '#scrollStart',
-				// 	duration: 500,
-				// 	loglevel: 3,
-				// })
-				// .setPin(`#${this.circles[i].id}`)
-				// .addIndicators()
-				// .addTo(this.smController);
-			}
+		console.log(this.circles);
+		for (var i = this.circles.length -1; i > 0; i--) {
+			const smScene = new ScrollMagic.Scene({
+				triggerElement: '#scrollStart',
+				duration: 500,
+				// loglevel: 3,
+			})
+			.setPin(`#circle-${i}`)
+			.addIndicators()
+			.addTo(this.smController);
 		}
 
 		this.setState({scrollMagicReady: true});
 	}
 
-	componentDidMount() {
-		if (this.circles.length == 0) this.drawCircles();
-		if (this.state.scrollMagicReady == false) this.initScrollMagic();
+	setSVGRect(isViewBox) {
+		if (isViewBox) return `0 0 ${this.maxRadius} ${this.maxRadius}`;
+		else return `${this.maxRadius}`;
 	}
 
 	render () {
@@ -73,16 +63,20 @@ export default class Welcome extends Component {
 					<div className="headerwrap">
 						<h1>In:Concert</h1>
 					</div>
-				</div>
-
-				<div className="scene" id="scrollStart">
-					<svg viewBox="0 0 0 0" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg">
-						<circle cx="50%" cy="50%" r="0" stroke="#bbb" strokeWidth="3" fill="none"/>
-					</svg>
 					<i className="fa fa-long-arrow fa-3x fa-rotate-180" aria-hidden="true"></i>
 				</div>
 
-				<div className="scene" id="scrollEnd"></div>
+				<div id="scrollStart"></div>
+				
+				<svg className="vinyl" 
+						viewBox={this.setSVGRect(true)}
+						width={this.setSVGRect(false)}
+						height={this.setSVGRect(false)}
+						preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg" >
+					{ this.circles.length == 0 ? this.drawCircles() : null }
+				</svg>
+
+				<div id="scrollEnd"></div>
 			</article>
 		)
 	}
