39a7913ee5347e8df590fc652c62a33fee209488
netzebandito <blake.netzeband@gmail.com>
2017-12-13 20:59:31 -0700

    basic app creation

---
+++ imports/ui/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
+export default class App extends Component {
+	render() {
+		return (
+			<h1>
+				HIII
+			</h1>
+		);
+	}
+}
